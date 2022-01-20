8ed2a5f1cf1d1fb8ddd243069f1813b5695cdc40
tomaszpankowski <pankowski.email@gmail.com>
2022-01-20 19:33:06 +0100

    modified routing

---
+++ src/views/bike.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class Bike extends Component{
+    render(){
+        return(        
+            <Container fluid className="bike-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default Bike;
